321e01e9dfe19ab47b2ac7d11d7ba261075fa4d4
xSquin <116567088+xSquin@users.noreply.github.com>
2022-12-12 08:24:03 +0100

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,17 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      today: {},
-      days: [],
-      day1: {},
-      day2: {},
-      day3: {},
-      day4: {},
-      day5: {}
+      today: {city: '', date: '', temp: '', description: '', pressure: '', wind: '', icon: ''},
+      days: [0, 0, 0, 0, 0]
     }
   }
   componentDidMount(){
-    window.addEventListener('load', this.tick())
+    this.tickToday()
+    this.tickDays()
   }
-  tick = async () =>{
-    const apiKey = `876b90b6ee972e22425ad62294ec52e6`;
-    const apiToday = `https://api.openweathermap.org/data/2.5/weather?q=Olsztyn&appid=${apiKey}&units=metric&lang=pl`;
-    const apiOtherDays = `http://api.openweathermap.org/data/2.5/forecast?q=Olsztyn&appid=${apiKey}&units=metric&lang=pl`
-
-    await fetch(apiToday)
+  tickToday = () => {
+    const apiToday = `https://api.openweathermap.org/data/2.5/weather?q=Olsztyn&appid=876b90b6ee972e22425ad62294ec52e6&units=metric&lang=pl`;
+    fetch(apiToday)
       .then(response => response.json())
       .then(result => {
         const time = new Date().toLocaleString()
@@ -34,15 +27,17 @@ class App extends Component {
             pressure: result.main.pressure,
             wind: result.wind.speed,
             icon: result.weather[0].icon
-          }  
+          }
         })
       })
-    
-    await fetch(apiOtherDays)
+  }
+  tickDays = () => {
+    const apiOtherDays = `http://api.openweathermap.org/data/2.5/forecast?q=Olsztyn&appid=876b90b6ee972e22425ad62294ec52e6&units=metric&lang=pl`;
+    fetch(apiOtherDays)
       .then(response => response.json())
       .then(result => {
-        let values = {date: '', temp: '', description: '', icon: ''}
-        let arr = [];
+        let values = {}
+        let days = [];
         for(let i = 0; i < 40; i++){
           const d = new Date(result.list[i].dt * 1000).toLocaleString("pl-PL");
           if(d.split(' ')[1].toString() === "13:00:00"){
@@ -52,90 +47,54 @@ class App extends Component {
               description: result.list[i].weather[0].description,
               icon: result.list[i].weather[0].icon
             }
-            arr.push(values);
+            days.push(values);
           } 
         }
-        if(this.state.days.length === 0){
-          this.setState({days:[...this.state.days, arr]})
-        }
+        this.setState({...this.state, days})
       })
-    if(this.state.days.length === 1){
-      this.setState({
-        day1: this.state.days[0][0],
-        day2: this.state.days[0][1],
-        day3: this.state.days[0][2],
-        day4: this.state.days[0][3],
-        day5: this.state.days[0][4]
-      })
-    }
   }
   render(){
-    console.log(this.state);
     return (
       <div className="App">
         <div className="weather-props">
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-
-            <div id="column1">
-                <div className="city">{this.state.today.city}</div>
-                <div className="date">
-                    <span className="material-symbols-outlined">schedule</span>
-                    {this.state.today.date}</div>
+            <div id="row-weather-props">
+              <div className="city">{this.state.today.city}</div>
+              <div className="date-today">
+                <span className="material-symbols-outlined">schedule</span>
+                {this.state.today.date}
+              </div>
             </div>
-            <div id="column2">
-                <div className="temp">
-                    <span className="material-symbols-outlined">thermometer</span>
-                    {this.state.today.temp} <span className="temp-text">o</span>C
-                </div>
-                <div className="description"><img className="description-img" src={`http://openweathermap.org/img/wn/${this.state.today.icon}@2x.png`} alt={this.state.today.description}></img>{this.state.today.description}</div>
+            <div id="row-weather-props">
+              <div className="temp-today">
+                <span className="material-symbols-outlined">thermometer</span>
+                {this.state.today.temp} <span className="temp-text">o</span>C
+              </div>
+              <div className="description-today">
+                <img className="description-img" src={`http://openweathermap.org/img/wn/${this.state.today.icon}@2x.png`} alt={this.state.today.description}></img>
+                {this.state.today.description}
+              </div>
             </div>
-            <div id="column3">
-            <div className="pressure">{this.state.today.pressure} hPa</div>
-                <div className="wind">
-                    <span className="material-symbols-outlined air">air</span>
-                    {this.state.today.wind} m/s</div>
+            <div id="row-weather-props">
+              <div className="pressure-today">
+                {this.state.today.pressure} hPa
+              </div>
+              <div className="wind-today">
+                <span className="material-symbols-outlined air">air</span>
+                {this.state.today.wind} m/s
+              </div>
             </div>
-            
-            <div id="column4">
+            <div id="row-weather-props">
+              {this.state.days.map(e => 
                 <div className="box-other-days">
-                    <div className="date-o">{this.state.day1.date}</div>
-                    <div className="temp-o">
-                      <span className="material-symbols-outlined thermometer">thermometer</span>
-                      {this.state.day1.temp}<span className="temp-text">o</span>C</div>
-                    <img className="description-img-o" src={`http://openweathermap.org/img/wn/${this.state.day1.icon}@2x.png`} alt={this.state.day1.description}></img>
+                  <div className="date-other-days">{e.date}</div>
+                  <div className="temp-other-days">
+                    <span className="material-symbols-outlined thermometer">thermometer</span>
+                    {e.temp}<span className="temp-text">o</span>C
+                  </div>
+                  <img src={`http://openweathermap.org/img/wn/${e.icon}@2x.png`} alt={e.description}></img>
                 </div>
-                <div className="box-other-days">
-                    <div className="date-o">{this.state.day2.date}</div>
-                    <div className="temp-o">
-                      <span className="material-symbols-outlined thermometer">thermometer</span>
-                      {this.state.day2.temp}<span className="temp-text">o</span>C
-                      </div>
-                    <img className="description-img-o" src={`http://openweathermap.org/img/wn/${this.state.day2.icon}@2x.png`} alt={this.state.day2.description}></img>
-                </div>
-                <div className="box-other-days">
-                    <div className="date-o">{this.state.day3.date}</div>
-                    <div className="temp-o">
-                      <span className="material-symbols-outlined thermometer">thermometer</span>
-                      {this.state.day3.temp}<span className="temp-text">o</span>C
-                    </div>
-                    <img className="description-img-o" src={`http://openweathermap.org/img/wn/${this.state.day3.icon}@2x.png`} alt={this.state.day3.description}></img>
-                </div>
-                <div className="box-other-days">
-                    <div className="date-o">{this.state.day4.date}</div>
-                    <div className="temp-o">
-                      <span className="material-symbols-outlined thermometer">thermometer</span>
-                      {this.state.day4.temp}<span className="temp-text">o</span>C
-                    </div>
-                    <img className="description-img-o" src={`http://openweathermap.org/img/wn/${this.state.day4.icon}@2x.png`} alt={this.state.day4.description}></img>
-                </div>
-                <div className="box-other-days">
-                    <div className="date-o">{this.state.day5.date}</div>
-                    <div className="temp-o">
-                      <span className="material-symbols-outlined thermometer">thermometer</span>
-                      {this.state.day5.temp}<span className="temp-text">o</span>C
-                    </div>
-                    <img className="description-img-o" src={`http://openweathermap.org/img/wn/${this.state.day5.icon}@2x.png`} alt={this.state.day5.description}></img>
-                </div>
+              )}
             </div>
         </div>
       </div>
